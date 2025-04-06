be4c6e346c2db8da07d546ce61737947b5e85511
ElCesarSP <cs.loko0125@gmail.com>
2025-04-05 22:31:20 -0300

    refactor(modificacao para funcionamento): Modicacao e alteraçoes para bom funcinamento do nosso codigo

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,14 @@ const port = 3000;
 
 const carroRouter = require("./routes/Carro/carroRouter");
 const eventoRouter = require("./routes/Evento/eventoRouter");
+const motoristaRouter = require("./routes/Motorista/motoristaRouter");
 
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
 app.use("/api", carroRouter);
 app.use("/api", eventoRouter);
+app.use("/api", motoristaRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
