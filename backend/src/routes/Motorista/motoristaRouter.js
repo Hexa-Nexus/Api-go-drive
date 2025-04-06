b28308fc3e672928c814749323ab7e3d6428bb2f
ElCesarSP <cs.loko0125@gmail.com>
2025-04-06 13:01:54 -0300

    refactor(controllerMotorista, routerMotorista): Modificações de update de update motorista e buscar por ID

--- backend/src/routes/Motorista/motoristaRouter.js
+++ backend/src/routes/Motorista/motoristaRouter.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.post("/motoristas", MotoristaCotroller.criar_motorista);
 router.get("/motoristas", MotoristaCotroller.obter_todos_motoristas);
 router.get("/motorista", MotoristaCotroller.obter_motorista_por_cpf);
+router.get("/motorista/:id", MotoristaCotroller.obter_motorista_por_id);
+router.put("/motorista/:id", MotoristaCotroller.atualizar_motorista);
 router.delete("/motorista/:id", MotoristaCotroller.deletar_motorista);
 
 module.exports = router;
