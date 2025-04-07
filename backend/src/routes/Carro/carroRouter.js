b7fda9e0e89a6760d5cb9642839892dfbc171c51
Josellyto <josellyto_@hotmail.com>
2025-04-06 23:20:37 -0300

    feat(controller-routes): update veiculo

--- backend/src/routes/Carro/carroRouter.js
+++ backend/src/routes/Carro/carroRouter.js
@@ -5,5 +5,6 @@ const CarroController = require("../../controllers/Carro/carroController");
 // Definições de rotas para carro
 router.post("/carros", CarroController.criandoCarro);
 router.get("/carros", CarroController.obter_todos_carros);
+router.put("/carros/:id", CarroController.atualizar_carro);
 
 module.exports = router;
